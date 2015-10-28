82a9c3a5ce58bb605b2ea36a6da0c002c37378c9
karthikbox <karthik.gakkidi@gmail.com>
2015-10-28 13:27:12 -0400

    master cache setup and evaluated. Added results directory.

--- packing/Simulation.java
+++ packing/Simulation.java
@@ -1,4 +1,5 @@
 import java.lang.Double;
+import java.lang.InterruptedException;
 import java.lang.System;
 import java.util.*;
 import java.io.BufferedReader;
@@ -15,6 +16,7 @@ class Simulation{
 
 
     static LRUCache[] caches;
+    static LRUCache masterCache;
     static String base="/home/karthik/cache-benchmark/lirs-trc/";
     static HashMap<Integer, Integer> unique_ht_count; // do not use this for knapsack value
     static ArrayList<Integer> unique_al;
@@ -23,6 +25,8 @@ class Simulation{
     static long seed=0xDEADBEEF; // seed for random number generator
     static int wt_max=50;
     static int MAX=10000;
+    static double knapsackWeight=0.1; // as percentage
+    static double masterKnapsackWeight=knapsackWeight*10;
 
     public static void main(String[] args) throws IOException{
         initCaches(10, 3000);
@@ -32,6 +36,107 @@ class Simulation{
         runTestTraceAgainstCachesUsing("sprite-test.trc");
         ArrayList<Boolean[]> hotDataOfAllCaches=doKnapSackOnCaches();
         runTestTraceAgainstKnapsacksUsing("sprite-test.trc");
+        //create a master knapsack
+        masterCache=mergeAllCachesAndInitCountOrder();
+        //run knapsack on this mastercace with size 20%
+        int w=(int)Math.ceil(masterKnapsackWeight*unique_al.size()*25);
+        int n=masterCache.cache_order.size();
+        double[][] table=new double[23000][5000];
+        boolean[][] sol=new boolean[23000][5000];
+        boolean[] taken=new boolean[23000];
+        for(double[] t:table) {
+            Arrays.fill(t,0);
+        }
+        for(boolean[] t:sol){
+            Arrays.fill(t,false);
+        }
+        Arrays.fill(taken,false);
+        System.out.println("MASTER CACHE"+" w="+w+" n="+n);
+        runKnapsackOnMaster(masterCache, w, n, table, sol, taken);
+        masterCache.initHotDataMapFromTakenArray(taken);
+        //runTestTrace against master knapsack
+        runTestTraceAgainstMasterKnapsackUsing("sprite-test.trc");
+    }
+
+    public static void runTestTraceAgainstMasterKnapsackUsing(String file)  throws IOException{
+        BufferedReader r = new BufferedReader(new FileReader(base + "/" + file));
+        String line;
+        int hits=0,miss=0;
+        int total_hits=0;
+        int total_miss=0;
+        while((line=r.readLine())!=null){
+            int a=Integer.parseInt(line);
+            if(masterCache.hotDataSetFromTakenArray.contains(a)){
+                hits++;
+                total_hits++;
+            }
+            else{
+                miss++;
+                total_miss++;
+            }
+        }
+        String str="";
+        str+=String.format("%d,%d,%f,%d\n",hits,miss,((float)hits/(hits+miss)),masterCache.hotDataSetFromTakenArray.size());
+        //System.out.println("total hits="+total_hits+" total miss="+total_miss+" total hitrate="+((float)total_hits/(total_hits+total_miss)));
+        System.out.println("master cache stats");
+        System.out.println(str);
+        r.close();
+    }
+
+    static void runKnapsackOnMaster(LRUCache c,int W,int N,double[][] table,boolean[][] sol,boolean[] taken){
+        for(int i=0;i<=N;i++){
+            table[0][i]=0;
+        }
+        for(int i=0;i<=W;i++){
+            table[i][0]=0;
+        }
+        //System.out.println("1");
+        for(int i=1;i<=W;i++) {
+            //System.out.println("master cache w="+i+" <= "+W);
+            for (int j = 1; j <= N; j++) {
+                //System.out.println("master cache n="+j+" <= "+N);
+                double a1 = table[i][j - 1];// without j
+                double a2 = Integer.MIN_VALUE;
+                //System.out.println(" j="+j+" wight "+c.cache_order.get(j));
+                if (i >= weight(c,j))
+                {
+
+
+                    a2 = table[i - weight(c,j)][j - 1] + value(c,j);//with j
+                }
+                table[i][j] = Math.max(a1, a2);
+                sol[i][j] = a1 < a2;
+            }
+        }
+        //System.out.println("2");
+        int w=W;
+        for(int j=N;j>0;j--){
+            if(sol[w][j]==true){
+                taken[j]=true;
+                w=w-weight(c,j);
+                //System.out.println("w= "+w);
+            }
+            else{
+                taken[j]=false;
+            }
+        }
+    }
+
+    static LRUCache mergeAllCachesAndInitCountOrder(){
+        //iterate over every cache
+        //from each cache get the countOrder map
+        LRUCache master=new LRUCache(caches[0].capacity*10);//enough space for 10 caches...but we wont use this anyway
+        int count=1;
+
+        for(int i =0;i<caches.length;i++){
+            int start=1;
+            int end=caches[i].cache_order.size();
+            for(int t=start;t<=end;t++) {
+                master.cache_order.put(count,caches[i].cache_order.get(t));
+                count++;
+            }
+        }
+        return master;
     }
 
     public static ArrayList<Boolean[]> doKnapSackOnCaches(){
@@ -41,9 +146,9 @@ class Simulation{
         boolean[] taken=new boolean[5000];
         ArrayList<Boolean[]> hotDataOfAllCaches = new ArrayList<Boolean[]>();
         for(int i=0;i<caches.length; i++) {
-            int w=(int)Math.ceil(0.002*unique_al.size()*25);
+            int w=(int)Math.ceil(knapsackWeight*unique_al.size()*25);
             int n=caches[i].map.size();
-            //System.out.println("cache #"+i+" w="+w+" n="+n);
+            System.out.println("cache #"+i+" w="+w+" n="+n);
             for(double[] t:table) {
                 Arrays.fill(t,0);
             }
@@ -59,6 +164,7 @@ class Simulation{
     }
 
     public static int weight(LRUCache c,int a){
+
         if(c.cache_order.containsKey(a)){
             int key= c.cache_order.get(a).key;
             if(unique_ht_weight.containsKey(key)){
@@ -146,10 +252,10 @@ class Simulation{
         }
         String str="";
         for(int i=0;i<caches.length;i++){
-            //System.out.println("hot-cache #"+i+" hits="+hits[i]+" miss="+miss[i]+" hitrate="+((float)hits[i]/(hits[i]+miss[i]))+" cached items="+caches[i].hotDataSetFromTakenArray.size());
+            System.out.println("hot-cache #"+i+" hits="+hits[i]+" miss="+miss[i]+" hitrate="+((float)hits[i]/(hits[i]+miss[i]))+" cached items="+caches[i].hotDataSetFromTakenArray.size());
             str+=String.format("%d,%d,%d,%f,%d\n",i,hits[i],miss[i],((float)hits[i]/(hits[i]+miss[i])),caches[i].hotDataSetFromTakenArray.size());
         }
-        //System.out.println("total hits="+total_hits+" total miss="+total_miss+" total hitrate="+((float)total_hits/(total_hits+total_miss)));
+        System.out.println("total hits="+total_hits+" total miss="+total_miss+" total hitrate="+((float)total_hits/(total_hits+total_miss)));
         str+=String.format("%d,%d,%d,%f,%d\n",caches.length,total_hits,total_miss,((float)total_hits/(total_hits+total_miss)),10);
         System.out.println();
         System.out.println(str);
